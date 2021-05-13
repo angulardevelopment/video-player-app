c68bbcd604de03abb9f52fefff8dc2cbfa05fc83
DESKTOP-1ASQ7K6\sunny <sunny.gupta@docquity.com>
2021-05-13 22:56:21 +0530

    NEW component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VideospeedComponent } from './videospeed/videospeed.component';
+import { TimesComponent } from './times/times.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VideospeedComponent
+    VideospeedComponent,
+    TimesComponent
   ],
   imports: [
     BrowserModule,
